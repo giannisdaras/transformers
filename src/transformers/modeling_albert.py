4f0f2065b119c1e6c5086bcfef48f6477ac4b82c
Giannis Daras <daras.giannhs@gmail.com>
2020-04-23 18:35:16 +0300

    Modify albert code to enable smyrf

--- src/transformers/modeling_albert.py
+++ src/transformers/modeling_albert.py
@@ -194,6 +194,23 @@ class AlbertAttention(BertSelfAttention):
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.pruned_heads = set()
 
+        try:
+            if config.smyrf:
+                from smyrf.torch.attn import SmyrfAttention
+                self.n_hashes = config.n_hashes
+                self.q_cluster_size = config.q_cluster_size
+                self.k_cluster_size = config.k_cluster_size
+                self.r = config.r
+                self.smyrf = SmyrfAttention(n_hashes=self.n_hashes,
+                                            q_cluster_size=self.q_cluster_size,
+                                            k_cluster_size=self.k_cluster_size,
+                                            r=self.r,
+                                            dropout=config.attention_probs_dropout_prob)
+            else:
+                self.smyrf = False
+        except:
+            self.smyrf = False
+
     def prune_heads(self, heads):
         if len(heads) == 0:
             return
@@ -226,27 +243,33 @@ class AlbertAttention(BertSelfAttention):
         key_layer = self.transpose_for_scores(mixed_key_layer)
         value_layer = self.transpose_for_scores(mixed_value_layer)
 
-        # Take the dot product between "query" and "key" to get the raw attention scores.
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        if attention_mask is not None:
-            # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
-            attention_scores = attention_scores + attention_mask
+        if not self.smyrf:
+            # Take the dot product between "query" and "key" to get the raw attention scores.
+            attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+            attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+            if attention_mask is not None:
+                # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+                attention_scores = attention_scores + attention_mask
 
-        # Normalize the attention scores to probabilities.
-        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+            # Normalize the attention scores to probabilities.
+            attention_probs = nn.Softmax(dim=-1)(attention_scores)
 
-        # This is actually dropping out entire tokens to attend to, which might
-        # seem a bit unusual, but is taken from the original Transformer paper.
-        attention_probs = self.dropout(attention_probs)
+            # This is actually dropping out entire tokens to attend to, which might
+            # seem a bit unusual, but is taken from the original Transformer paper.
+            attention_probs = self.dropout(attention_probs)
 
-        # Mask heads if we want to
-        if head_mask is not None:
-            attention_probs = attention_probs * head_mask
+            # Mask heads if we want to
+            if head_mask is not None:
+                attention_probs = attention_probs * head_mask
 
-        context_layer = torch.matmul(attention_probs, value_layer)
+            context_layer = torch.matmul(attention_probs, value_layer)
 
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+            context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        else:
+            bs, nH, N, dim = query_layer.shape
+            context_layer_with_heads = self.smyrf(query_layer.reshape(-1, N, dim), key_layer.reshape(-1, N, dim), value_layer.reshape(-1, N, dim))
+            # concat heads
+            context_layer = context_layer_with_heads.reshape(query_layer.shape).permute(0, 2, 1, 3)
 
         # Should find a better way to do this
         w = (
